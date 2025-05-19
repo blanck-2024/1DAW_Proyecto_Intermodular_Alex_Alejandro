692cb379c0ad9cea8b954413d6554592197d8b49
Alejandro Puente <alejandropuentegimenez2@gmail.com>
2025-05-19 16:39:02 +0200

    Delete CarHub directory

--- CarHub/API/apirest--MySQL/src/main/java/apirest/apirest/repositories/ICompraVentaRepository.java
+++
@@ -1,8 +0,0 @@
-package apirest.apirest.repositories;
-import apirest.apirest.models.CompraVentaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICompraVentaRepository extends JpaRepository<CompraVentaModel,Long> {
-}
